7b77a1cdea4fb0d9a0fd8d976cb1d1b975202593
freedom12348 <123140554+freedom12348@users.noreply.github.com>
2023-01-19 20:23:51 -0800

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,41 @@
+import discord
+import responses
+
+
+async def send_message(message, user_message, is_private):
+    try:
+        response = responses.get_response(user_message)
+        await message.author.send(response) if is_private else await message.channel.send(response)
+
+    except Exception as e:
+        print(e)
+
+
+def run_discord_bot():
+    TOKEN = 'MTA2NTA1MjA2NTIyODkyMjg4MA.GTfvf8.IlQC1nVRXk5kVfL2s4FsjNRY7TDiLFa0eF5zjg'
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = discord.Client(intents=intents)
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now running!')
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+
+        print(f'{username} said: "{user_message}" ({channel})')
+
+        if user_message[0] == '?':
+            user_message = user_message[1:]
+            await send_message(message, user_message, is_private=True)
+        else:
+            await send_message(message, user_message, is_private=False)
+
+    client.run(TOKEN)
